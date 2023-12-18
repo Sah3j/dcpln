425525d5bfe3aacf7c543a6c695552488b7b2dee
Sahejdeep Singh <bboysahej@gmail.com>
2023-12-18 00:37:10 -0800

    pushing

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         destination:
         process.env.NODE_ENV === 'development'
           ? 'http://127.0.0.1:5000/api/:path*'
-          : 'https://dcpln.vercel.app/api/:path*',
+          : '/api/',
       }
     ]
   }
