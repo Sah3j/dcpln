0b14d58b3375119e5144c3019e414c92b5943281
Sahejdeep Singh <bboysahej@gmail.com>
2023-12-18 00:26:06 -0800

    fixing backend

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         destination:
         process.env.NODE_ENV === 'development'
           ? 'http://127.0.0.1:5000/api/:path*'
-          : '/api/',
+          : 'https://dcpln.vercel.app/api/:path*',
       }
     ]
   }
